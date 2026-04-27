4b3152d596b5f6a629197ccafc30c6bf389b1f1a
GistaLenggana <gista120508@gmail.com>
2026-04-27 16:15:50 +0700

    API Attendances

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AttendanceController;
 use App\Http\Controllers\Api\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('students', StudentController::class);
+Route::apiResource('attendances', AttendanceController::class)->only(['index', 'store']);
