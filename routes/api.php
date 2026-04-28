7337a9beaa80c8e81504c6345d4a4e765221e1e5
TristanMZ <rytmaramadhanm@gmail.com>
2026-04-28 08:05:32 +0700

    Integrasi route presensi, siswa, dan SP

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\Api\AttendanceController;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\WarningLetterController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('students', StudentController::class);
 Route::apiResource('attendances', AttendanceController::class)->only(['index', 'store']);
-Route::apiResource('warning-letters', \App\Http\Controllers\Api\WarningLetterController::class);
+Route::apiResource('warning-letters', WarningLetterController::class);
