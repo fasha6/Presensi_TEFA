75ecfd124e7c0b5f75ec51d4b187e5e72a1b1d07
TristanMZ <rytmaramadhanm@gmail.com>
2026-04-30 08:57:02 +0700

    menambahkan route untuk notification

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\Api\AttendanceController;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\WarningLetterController;
+use App\Http\Controllers\Api\NotificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('students', StudentController::class);
 Route::apiResource('attendances', AttendanceController::class)->only(['index', 'store']);
 Route::apiResource('warning-letters', WarningLetterController::class);
+Route::apiResource('notifications', NotificationController::class);
