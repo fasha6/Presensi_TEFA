8530a33a4378dc3f767fb9f774ff19fb5113285a
shellymuthiaa <muthiashelly113>
2026-06-30 07:28:17 +0700

    Mengubah data siswa XI PPL 1

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -10,18 +10,227 @@ class StudentSeeder extends Seeder
     public function run(): void
     {
         $students = [
-            ['nis' => '20240001', 'name' => 'Ahmad Rizki Maulana', 'class_name' => 'X PPL 1', 'major' => 'PPL', 'attendance_percentage' => 85, 'violations_count' => 4, 'warning_letters_count' => 1, 'risk_level' => 'medium', 'parent_name' => 'Bapak Maulana', 'parent_phone' => '081234567890'],
-            ['nis' => '20240002', 'name' => 'Siti Nurhaliza', 'class_name' => 'XI PPL 1', 'major' => 'PPL', 'attendance_percentage' => 92, 'violations_count' => 2, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Ibu Nur', 'parent_phone' => '081298765432'],
-            ['nis' => '20240003', 'name' => 'Budi Santoso', 'class_name' => 'XII TJK 3', 'major' => 'TJK', 'attendance_percentage' => 78, 'violations_count' => 6, 'warning_letters_count' => 2, 'risk_level' => 'high', 'parent_name' => 'Bapak Santoso', 'parent_phone' => '081276543210'],
-            ['nis' => '20240004', 'name' => 'Devi Anggraini', 'class_name' => 'XI AKL 3', 'major' => 'AKL', 'attendance_percentage' => 96, 'violations_count' => 0, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Ibu Anggraini', 'parent_phone' => '081265432109'],
-            ['nis' => '20240005', 'name' => 'Eko Prasetyo', 'class_name' => 'XII PM 2', 'major' => 'PM', 'attendance_percentage' => 88, 'violations_count' => 2, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Bapak Prasetyo', 'parent_phone' => '081254321098'],
-            ['nis' => '20240006', 'name' => 'Fatimah Zahra', 'class_name' => 'XI MPL 2', 'major' => 'MPL', 'attendance_percentage' => 90, 'violations_count' => 1, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Ibu Zahra', 'parent_phone' => '081243210987'],
-            ['nis' => '20240007', 'name' => 'Gilang Ramadhan', 'class_name' => 'XII TLG 2', 'major' => 'TLG', 'attendance_percentage' => 82, 'violations_count' => 5, 'warning_letters_count' => 1, 'risk_level' => 'medium', 'parent_name' => 'Bapak Ramadhan', 'parent_phone' => '081232109876'],
-            ['nis' => '20240008', 'name' => 'Hani Safitri', 'class_name' => 'XI TKF 3', 'major' => 'TKF', 'attendance_percentage' => 94, 'violations_count' => 1, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Ibu Safitri', 'parent_phone' => '081221098765'],
-            ['nis' => '20240009', 'name' => 'Irfan Hakim', 'class_name' => 'XII TLM 2', 'major' => 'TLM', 'attendance_percentage' => 87, 'violations_count' => 3, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Bapak Hakim', 'parent_phone' => '081210987654'],
-            ['nis' => '20240010', 'name' => 'Jihan Aulia', 'class_name' => 'XI DKV 1', 'major' => 'DKV', 'attendance_percentage' => 91, 'violations_count' => 1, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Ibu Aulia', 'parent_phone' => '081209876543'],
-            ['nis' => '20240011', 'name' => 'Kurnia Aji', 'class_name' => 'X TET 1', 'major' => 'TET', 'attendance_percentage' => 75, 'violations_count' => 7, 'warning_letters_count' => 2, 'risk_level' => 'high', 'parent_name' => 'Bapak Aji', 'parent_phone' => '081298761234'],
-            ['nis' => '20240012', 'name' => 'Lina Marlina', 'class_name' => 'XI TJK 2', 'major' => 'TJK', 'attendance_percentage' => 93, 'violations_count' => 1, 'warning_letters_count' => 0, 'risk_level' => 'low', 'parent_name' => 'Ibu Marlina', 'parent_phone' => '081287651234'],
+            [
+    'nis' => '242510397',
+    'name' => 'AGHRY ADRYANTO',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510398',
+    'name' => 'ANAS NASRULLOH',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510399',
+    'name' => 'AULIA NOVIA SHUANDHARI',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510400',
+    'name' => 'BAGJA SASTRA ATMANAGARA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510401',
+    'name' => "DHIIYA'ALI MALIK DZULFIQAR",
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510402',
+    'name' => 'FARDI FIRMANSYAH',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510403',
+    'name' => 'FERDIANSYAH',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510404',
+    'name' => 'FINSA ANGGA RAMADHAN',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510405',
+    'name' => 'GALIH ADIL DARMAWAN',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510406',
+    'name' => 'GAZLAN HAZIQE EL AKBAR',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510407',
+    'name' => 'HAURA SALSABIL',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510408',
+    'name' => 'IBNU FARREL HERMAWAN',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510409',
+    'name' => 'JENISA NURFADILLAH',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510410',
+    'name' => 'KAHVI RIZKY HARDIANSYAH',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510411',
+    'name' => 'M GIBRAN RASYAD BAIS SUKMANA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510412',
+    'name' => 'MOCHAMAD HAMBY ALFARIZI',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510413',
+    'name' => 'MOCHAMAD ARSY ZULFA NUR SAFIQ',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510414',
+    'name' => 'MUHAMMAD FATHIR AKBARRUSSALAM',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510415',
+    'name' => 'MUHAMMAD FAREL NUGRAHA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510416',
+    'name' => 'MUHAMMAD FASHA PRATAMA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510417',
+    'name' => 'MUHAMMAD RAFFA ANUGRAH SOPA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510418',
+    'name' => 'NAFIS IKHSAN',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510419',
+    'name' => 'RAFI RASYAD MUHAMMAD',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510420',
+    'name' => 'RAKA NURDIANA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510421',
+    'name' => 'REGINA AFRINIALI',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510422',
+    'name' => 'REHAN ALFARIDZI',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510423',
+    'name' => 'RIKI RUHIKMAT',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510424',
+    'name' => 'RUSTAMAN DELVIANOVA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510425',
+    'name' => 'SALMAN ROHIMAN',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510426',
+    'name' => 'SEPTIAN RAMADHAN',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510427',
+    'name' => 'SHABRINA NAFFISAH AYYASY',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510428',
+    'name' => 'SHELA PITRIANI',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510429',
+    'name' => 'SHELLY MUTHIA AZZAHRA',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '20240002',
+    'name' => 'Siti Nurhaliza',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510430',
+    'name' => 'TRISTAN MALIKA ZULKIFLI',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510431',
+    'name' => 'ZAKI ALGHANI RACHMAN',
+    'class_name' => 'XI PPL 1',
+],
+
+[
+    'nis' => '242510432',
+    'name' => 'ZEFI NURFADILAH',
+    'class_name' => 'XI PPL 1',
+],
         ];
 
         foreach ($students as $student) {
